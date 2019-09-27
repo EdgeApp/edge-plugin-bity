f4f56f9a016125c16690f53a4f7556eed63541b5
Allen Hujsak <allen@missionvi.com>
2019-09-27 16:48:34 -0500

    fixed styles and updated dependency

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -58,20 +58,11 @@ export const sceneMainContainer = {
   width: '100%'
 }
 
-export const sceneButtonBottom = {
-  display: 'flex',
-    position: 'relative',
-    flexShrink: 1,
-    minHeight: 60,
-    width: '100%',
-    maxHeight: 70,
-    backgroundColor: THEME.COLORS.ACCENT_ORANGE
-}
 export const containerSpinner = {
   display: 'flex',
-  flex: '1',
+  flex: 1,
   width: '100%',
-  height: '100%',
+  minHeight: '100vh',
   flexDirection: 'row',
   alignItems: 'center',
   justifyContent: 'space-around'
