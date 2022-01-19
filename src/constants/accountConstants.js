d8d67df134a3dabce4dad70810aa53bd6fe1fdd9
eliran zach <thehobbit85@gmail.com>
2022-01-18 22:17:55 -0800

    Use our CDN instead of wordpress

--- src/constants/accountConstants.js
+++ src/constants/accountConstants.js
@@ -4,16 +4,16 @@ export const PENDING = 'PENDING'
 export const APPROVED = 'APPROVED'
 export const POWERED_BY_LOGO = {
   email: 'support@bity.com',
-  logo: 'https://edge.app/wp-content/uploads/2019/09/poweredByLogoWhite.png',
+  logo: 'https://content.edge.app/BuySellPlugins/Bity/poweredByLogoWhite.png',
   width: 74,
   height: 30,
   useBackground: false
 }
 export const POWERED_BY_LOGO_DARK = {
   email: 'support@bity.com',
-  logo: 'https://edge.app/wp-content/uploads/2019/09/poweredByLogo-1.png',
+  logo: 'https://content.edge.app/BuySellPlugins/Bity/poweredByLogo.png',
   width: 74,
   height: 30,
   useBackground: false
 }
-export const MAIN_LOGO = 'http://edge.app/wp-content/uploads/2019/09/logo300.png'
+export const MAIN_LOGO = 'https://content.edge.app/BuySellPlugins/Bity/logo300.png'
